d4c0d87cddd893dbe324459e649c41004e175218
unknown <reviarga07@gmail.com>
2024-08-13 08:38:58 -0700

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(){
+//     if($(window).innerWidth() >= 751) {
+//     $("#navbarDropdown-show").mouseenter(function(){
+//       $("#dropdown-menu-1").show();
+//       $("#dropdown-menu-1").mouseleave(function(){
+//         $("#dropdown-menu-1").hide();
+//       });
+//     });} else {
+//       $("#navbarDropdown-show").click(function(){
+//         $("#dropdown-menu-1").toggle();
+//       });
+//     }
+
+//     if($(window).innerWidth() >= 751) {
+//         $("#navbarDropdown").mouseenter(function(){
+//           $("#dropdown-menu-2").show();
+//           $("#dropdown-menu-2").mouseleave(function(){
+//             $("#dropdown-menu-2").hide();
+//           });
+//         });} else {
+//           $("#navbarDropdown").click(function(){
+//             $("#dropdown-menu-2").toggle();
+//           });
+//         }
+//   });
